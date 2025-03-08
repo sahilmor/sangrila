15a353ef720cf0db8197384e9647a8a6519fe49b
sahilmor <mor.sahil05.28@gmail.com>
2025-03-09 04:11:30 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["images.unsplash.com", "encrypted-tbn0.gstatic.com"],
-  }
+  },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
 };
 
 export default nextConfig;
